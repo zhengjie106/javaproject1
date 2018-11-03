80495aff16e6f4c122c2d12db7b61bf9faf44bea
Liver <liver@ihewakatekiMacBook-Air.local>
2018-11-03 15:49:56 +0800

    updated

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
   public static void main(String[] args){
     for(int i=0; i<=10; i++){
-      System.out.println("Hello world ... " +i);
+      System.out.println("Hello world dsafdsaf... " +i);
     }
   }
 }
